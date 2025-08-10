3f8d3765bb23b98afaafddb75132147c8d79797b
mysangle <mysangle@users.noreply.github.com>
2025-08-11 00:12:05 +0900

    add spa routing rewrite

--- supabase/src/store/supabase.ts
+++ supabase/src/store/supabase.ts
@@ -1,13 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-let supabaseUrl;
-let supabaseAnonKey;
-if (typeof process !== "undefined") {
-    supabaseUrl = process.env.SUPABASE_URL;
-    supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
-} else {
-    supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-    supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-}
-
-export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+export const supabaseClient = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
