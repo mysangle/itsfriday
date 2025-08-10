f18e33a957d8d96d5f0a1e5837a5302371ae9093
mysangle <mysangle@users.noreply.github.com>
2025-08-10 23:51:43 +0900

    access environment variables by environment

--- supabase/src/store/supabase.ts
+++ supabase/src/store/supabase.ts
@@ -1,3 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabaseClient = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
+let supabaseUrl;
+let supabaseAnonKey;
+if (typeof process !== "undefined") {
+    supabaseUrl = process.env.SUPABASE_URL;
+    supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+} else {
+    supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+    supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+}
+
+export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
